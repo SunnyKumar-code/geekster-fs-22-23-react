5fb416a187df7cba6d858aa4b762bb8655c2d4a3
Divyansh Moonat <divyanshmoonat@outlook.com>
2025-02-05 22:04:13 +0530

    05-02-2025 | Nodejs Intro & Setup

---
+++ fs-22-23/module_6/classes/nodejs/index.js
@@ -0,0 +1,13 @@
+/**
+ * Import & Export Syantx
+ * ESM
+ *  import axios from "axios";
+ *  export default myModule;
+ *  export const myFn = () => {};
+ * CJS
+ *  const axios = require("axios");
+ *  modue.exports = myModule;
+ *  module.exports = {
+ *      myFn
+ *  }
+ */
